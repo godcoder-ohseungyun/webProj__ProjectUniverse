612518a4c4863ae0412aa3c4546f816046f8e72d
ohseungyun <afrada123@likelion.org>
2022-02-15 21:48:58 +0900

    CORS 설정 WebConfig

--- PU/pu-service/src/main/java/PU/puservice/Config/WebConfig.java
+++ PU/pu-service/src/main/java/PU/puservice/Config/WebConfig.java
@@ -1,6 +1,9 @@
 package PU.puservice.Config;
 
+import PU.puservice.interceptor.LoginCheckInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -10,17 +13,27 @@ public class WebConfig implements WebMvcConfigurer {
      * 인터셉터 등록
      * interceptor/LoginCheckInterceptor.java
      */
-//    @Override
-//    public void addInterceptors(InterceptorRegistry registry) {
-//
-//        registry.addInterceptor(new LoginCheckInterceptor())
-//                .order(1)
-//                .addPathPatterns("/**")
-//                .excludePathPatterns(
-//                        "/", "/join", "/login", "/logout",
-//                        "/css/**", "/*.ico", "/error"
-//                );
-//    }
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/", "/join", "/login", "/logout",
+                        "/css/**", "/*.ico", "/error"
+                );
+    }
 
 
+    /**
+     * CORS 설정
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8081");  //8081로 부터 오는 모든 요청에 CORS 적용
+
+
+    }
 }
