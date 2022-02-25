62ae3e815c0021a4cd2ac660b5ad336ceb23a218
ohseungyun <afrada123@likelion.org>
2022-02-25 16:44:02 +0900

    스웨거 + 인터셉터 미적용 발생 해결방법 모색중  -> 일단 주석처리함

--- PU/pu-service/src/main/java/PU/puservice/Config/WebConfig.java
+++ PU/pu-service/src/main/java/PU/puservice/Config/WebConfig.java
@@ -13,17 +13,17 @@ public class WebConfig implements WebMvcConfigurer {
      * 인터셉터 등록
      * interceptor/LoginCheckInterceptor.java
      */
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-
-        registry.addInterceptor(new LoginCheckInterceptor())
-                .order(1)
-                .addPathPatterns("/**")
-                .excludePathPatterns(
-                        "/", "/join", "/login", "/logout",
-                        "/css/**", "/*.ico", "/error" ,"/swagger-ui/index.html#/"
-                );
-    }
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//
+//        registry.addInterceptor(new LoginCheckInterceptor())
+//                .order(1)
+//                .addPathPatterns("/**")
+//                .excludePathPatterns(
+//                        "/", "/join", "/login", "/logout",
+//                        "/css/**", "/*.ico", "/error"
+//                );  //스웨거 /swagger-ui/index.html는 왜 제외 안되는???
+//    }
 
     private final long MAX_AGE_SECS = 3600;
     /**
